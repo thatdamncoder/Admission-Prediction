58d1ea150bbbd7c53b3e6c848bf99fa290ed2ed2
thatdamncoder <144144009+thatdamncoder@users.noreply.github.com>
2023-12-12 22:39:36 +0530

    Update 6_Code.py encoder error fixed

--- pages/6_Code.py
+++ pages/6_Code.py
@@ -3,12 +3,12 @@ import streamlit as st
 st.title("What works behind the scenes?")
 
 tab1, tab2 ,tab3,tab4,tab5,tab6,tab7= st.tabs(["Training Code", 
-                                     "Streamlit Home.py Code",
-                                     "Streamlit Prediction.py Code",
-                                     "Streamlit Result.py Code",
-                                     "Streamlit About.py Code",
-                                     "Streamlit Code.py Code",
-                                     "Streamlit Dataset.py Code"])
+                                     "Home.py Code",
+                                     "Prediction.py Code",
+                                     "Result.py Code",
+                                     "About.py Code",
+                                     "Code.py Code",
+                                     "Dataset.py Code"])
 
 with tab1:
     col1,col2=st.columns([1,3])
@@ -16,7 +16,7 @@ with tab1:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("Python Training Code")
-        with open("MLCode.txt", "r") as f:
+        with open("MLCode.txt",encoding="utf8") as f:
             code = f.read()
             st.code(code,language="python")
 
@@ -26,7 +26,7 @@ with tab2:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The Home Page")
-        with open("1_Home.py", "r") as f:
+        with open("1_Home.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
    
@@ -36,7 +36,7 @@ with tab3:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The Prediction Page")
-        with open("pages/3_Prediction.py", "r") as f:
+        with open("pages/3_Prediction.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
 
@@ -47,7 +47,7 @@ with tab4:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The Result Page")
-        with open("pages/4_Result.py", "r") as f:
+        with open("pages/4_Result.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
             
@@ -57,7 +57,7 @@ with tab5:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The About Page")
-        with open("pages/8_About.py", "r") as f:
+        with open("pages/8_About.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
 
@@ -67,7 +67,7 @@ with tab6:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The Code Page (you're on this page)")
-        with open("pages/6_Code.py", "r") as f:
+        with open("pages/6_Code.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
 
@@ -77,7 +77,7 @@ with tab7:
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
     with col2:
         st.header("The Dataset Page")
-        with open("pages/7_Dataset.py", "r") as f:
+        with open("pages/7_Dataset.py", encoding="utf8") as f:
             code = f.read()
             st.code(code)
 
