80c31b6ab873b3746e8301ee764b46c1d7fa13f2
thatdamncoder <144144009+thatdamncoder@users.noreply.github.com>
2024-01-10 00:33:53 +0530

    Update 7_Dataset.py

--- pages/7_Dataset.py
+++ pages/7_Dataset.py
@@ -1,14 +1,6 @@
-import joblib
 import numpy as np
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
 
 st.title("Take a look at the data!")
 st.write("Data size:  500 x 8 ")
